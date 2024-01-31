7ae58169d9f42dbc181d8cc5d9d41a07a4c28a9f
RickVellingaa <rickvellinga2408@gmail.com>
2024-01-31 15:11:31 +0100

    Redirect naar login formulier wanneer je niet begint ingelogd (closes #9 #4 )
    Je wordt nu geredirect naar de login pagina wanneer je probeert om naar de dashboard te gaan als je niet bent ingelogd.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { useAuth } from "../AuthContext";
+import { useNavigate } from "react-router-dom";
+
 function Dashboard() {
+  const { loggedIn } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loggedIn) {
+      navigate("/login");
+    }
+  }, [loggedIn, navigate]);
+
   return (
     <main>
       <section className="dashboard">
