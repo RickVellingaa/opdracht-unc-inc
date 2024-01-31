f29b2f2a38046a5197d6a2ca7179cb271f7784f0
RickVellingaa <rickvellinga2408@gmail.com>
2024-01-31 12:35:26 +0100

    useStates aangemaakt voor FormHandle
    states aangemaakt voor de form die ik uiteindelijk aan de handleLogin kan meegeven voor de Authenticatie

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 function Form() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {};
+
   return (
     <section className="login-form">
       <h2>Login bij UNC INC</h2>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <label htmlFor="gebruikersnaam">Gebruikersnaam</label>
-        <input type="text" id="gebruikersnaam" required />
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          id="gebruikersnaam"
+          required
+        />
         <label htmlFor="wachtwoord">Wachtwoord</label>
-        <input type="password" id="wachtwoord" required />
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          id="wachtwoord"
+          required
+        />
         <input type="submit" value="Login" />
       </form>
     </section>
